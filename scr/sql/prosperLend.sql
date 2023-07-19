16f57725ee508e72d12cf6d4ffe7ba042c1d5d7d
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-07-19 14:32:03 +0100

    Add table messages

--- scr/sql/prosperLend.sql
+++ scr/sql/prosperLend.sql
@@ -1,3 +1,14 @@
+DROP DATABASE IF EXISTS TestProjectProsperLend;
+CREATE DATABASE TestProjectProsperLend;
+USE TestProjectProsperLend;
+
 DROP DATABASE IF EXISTS ProjectProsperLend;
 CREATE DATABASE ProjectProsperLend;
 USE ProjectProsperLend;
+
+CREATE TABLE Messages (
+  messageId INT AUTO_INCREMENT,
+  userLoginId INT, CONSTRAINT FOREIGN KEY (userLoginId) REFERENCES Business(userLoginId) ON UPDATE CASCADE ON DELETE CASCADE,
+  message VARCHAR(255),
+  CONSTRAINT PK_messageId PRIMARY KEY (messageId));
+  
