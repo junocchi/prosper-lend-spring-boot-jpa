565416650a5b1dd2295088f12ad7e52f2fe32eab
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-07-20 10:08:02 +0100

    Add Transaction and Message dto and dao

---
+++ ProsperLend/src/main/java/com/dto/entity/Transaction.java
@@ -0,0 +1,63 @@
+package com.dto.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Transactions")
+public class Transaction {
+
+	@Id
+	private int transactionId;
+	private int loanId;
+	private double amount;
+	private Date transactionDate;
+
+	public Transaction() {
+		super();
+	}
+
+	public Transaction(int transactionId, int loanId, double amount, Date transactionDate) {
+		super();
+		this.transactionId = transactionId;
+		this.loanId = loanId;
+		this.amount = amount;
+		this.transactionDate = transactionDate;
+	}
+
+	public int getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public int getLoanId() {
+		return loanId;
+	}
+
+	public void setLoanId(int loanId) {
+		this.loanId = loanId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+
+}
