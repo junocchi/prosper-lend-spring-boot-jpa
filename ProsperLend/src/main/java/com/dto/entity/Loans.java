b2d86e4b6c41a5968383b9e840371f3f69f1d3fa
Dominique Walker <DW629@live.mdx.ac.uk>
2023-07-20 10:09:52 +0100

    loans dto

--- ProsperLend/src/main/java/com/dto/entity/Loans.java
+++ ProsperLend/src/main/java/com/dto/entity/Loans.java
@@ -1,5 +1,31 @@
 package com.dto.entity;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 public class Loans {
+	
+	@Id
+	int loanID;
+	BigDecimal amount;
+	BigDecimal interest;
+	int businessID;
+	Date loanDate;
+	@OneToMany
+	 private List<Transaction> transactionList = new ArrayList<Transaction>();
+	
+	
+	
+//	loanID INT PRIMARY KEY,
+//	  loanStatus VARCHAR(10),
+//	  amount DECIMAL(10, 2),
+//	  interest DECIMAL(5, 2),
+//	  businessID INT,
+//	  loanDate DATE,
+//	  FOREIGN KEY (businessID) REFERENCES businesses(businessID)
+//	
 
 }
