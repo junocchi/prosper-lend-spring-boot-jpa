7d3c284f2f1e1f2489311a3e1557b6b484bd40a0
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-07-20 14:34:21 +0100

    Service layer update

---
+++ ProsperLend/src/main/java/com/model/service/UserLoginService.java
@@ -0,0 +1,16 @@
+package com.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dto.entity.UserLogin;
+
+public interface UserLoginService {
+	public UserLogin addUser(UserLogin user);
+	public UserLogin getUserById(int id);
+	public UserLogin getUserByUsername(String username);
+	public List<UserLogin> getAllUsers();
+	public boolean deleteUserById(int id);
+	public boolean updatePassword(UserLogin user);
+}
